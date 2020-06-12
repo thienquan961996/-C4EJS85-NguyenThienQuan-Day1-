5a80d70b88563f8411650f0fad4c0663c7d90f83
nguyenthienquan <nguyenthienquan1996@gmail.com>
2020-06-12 21:38:56 +0700

    Bai Tap Day 1

---
+++ DAY1/index.js
@@ -0,0 +1,39 @@
+// console.log("bai3+4");
+// let x = 'Coding is Great';
+// console.log(x);
+// let y =0;
+// console.log(y);
+// let s= x.toLowerCase();
+// console.log(s);
+
+// x = 'Coding might not be easy, but still great';
+// console.log(x);
+// y= 16;
+// console.log(y);
+// y++;
+// console.log(y);
+
+// console.log("bai5");
+// alert('Wish you a good day');
+
+// console.log("bai6");
+// let name =prompt('hi , your name?');
+// alert("hello" + " " + name );
+
+// console.log("bai7");
+// let fname = prompt(' what your first name :');
+// let lname = prompt(' what your last name :');
+// alert("hello" + " " + lname + " " + fname);
+
+// console.log("bai8");
+// let x = prompt("Nhap chieu dai canh : ");
+// alert("dien tich hinh vuong la " + x*x);
+
+// console.log("bai9");
+// let x = prompt("ban kinh hinh tron la : ");
+// alert("dien tich hinh tron la " + x*x*3.14);
+
+// console.log("bai10");
+// let x = prompt("nhap gia tri do C :");
+// let y = x*1.8 + 32;
+// alert("gia tri do F la " + y  );
